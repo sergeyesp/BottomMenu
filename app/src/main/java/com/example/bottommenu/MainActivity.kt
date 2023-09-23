165159f4d352d6490d0b46705b509c563ce4122b
sergeyesp <sergey_esp@i.ua>
2023-09-23 19:59:52 +0300

    Lesson

--- app/src/main/java/com/example/bottommenu/MainActivity.kt
+++ app/src/main/java/com/example/bottommenu/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.bottommenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.example.bottommenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        binding.bottomNavView.setupWithNavController(navController)
+        binding.bottomNavView.itemIconTintList = null
     }
 }
